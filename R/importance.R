664e5826ac57295385bfd1477b3e8d0cb369b77d
Jonny Law <law.jonny@googlemail.com>
2019-08-31 09:30:04 +0100

    fix imports

--- R/importance.R
+++ R/importance.R
@@ -10,7 +10,7 @@
 shuffle_column <- function(data, j) {
   n <- nrow(data)
   selected_values <- sample.int(n = n, replace = F, size = n)
-  data[, j] = data[selected_values, j]
+  data[, j] <- data[selected_values, j]
   data
 }
 
@@ -27,8 +27,8 @@ shuffle_column <- function(data, j) {
 #' @examples
 calculate_metric <- function(model, new_data, truth, metric) {
   predict(model, new_data = new_data) %>%
-    bind_cols(new_data) %>%
-    metric(truth = !!ensym(truth), estimate = .pred)
+    dplyr::bind_cols(new_data) %>%
+    metric(truth = !!rlang::ensym(truth), estimate = .pred)
 }
 
 #' Title
@@ -59,37 +59,37 @@ calculate_importance <- function(base_perf, new_perf) {
 #'
 #' @examples
 permutation_importance <- function(model, new_data, truth, metric, n = 1) {
-  base_performance <- calculate_metric(model, new_data, !!ensym(truth), metric)
+  base_performance <- calculate_metric(model, new_data, !!rlang::ensym(truth), metric)
 
   # Remove the outcome column
   without_outcome <- new_data %>%
-    select(-!!enquo(truth))
+    dplyr::select(-!!enquo(truth))
   m <- ncol(without_outcome)
   importance <- matrix(NA_real_, nrow = m, ncol = n)
 
   # for each column in the predictors shuffle and calculate the metric
   for (j in seq_len(m)) {
-    for (i in seq_len(n)) {
-      shuffled_data <- bind_cols(shuffle_column(without_outcome, j), new_data %>% select(!!enquo(truth)))
-      new_performance <- calculate_metric(model, shuffled_data, !!ensym(truth), metric)
-      importance[j, i] <- calculate_importance(base_performance, new_performance)
-    }
+    importance[j, ] <- replicate(n, expr = {
+      shuffled_data <- dplyr::bind_cols(shuffle_column(without_outcome, j),
+                                 new_data %>% dplyr::select(!!enquo(truth)))
+      new_performance <- calculate_metric(model, shuffled_data, !!rlang::ensym(truth), metric)
+      calculate_importance(base_performance, new_performance)
+    })
   }
 
   if (n == 1) {
-    tibble(
+    tibble::tibble(
       feature = colnames(without_outcome),
       importance = importance[, 1]
     )
   } else {
-    tibble(
+    tibble::tibble(
       feature = colnames(without_outcome),
       mean_importance = apply(importance, 1, mean),
       lower_importance = apply(importance, 1, quantile, probs = 0.05),
       upper_importance = apply(importance, 1, quantile, probs = 0.95)
     )
   }
-
 }
 
 #' Title
@@ -102,8 +102,7 @@ permutation_importance <- function(model, new_data, truth, metric, n = 1) {
 #' @examples
 plot_importance <- function(importance) {
   importance %>%
-    mutate(feature = forcats::fct_reorder(feature, -importance)) %>%
-    ggplot(aes(x = feature, weight = importance)) +
-    geom_bar()
-
+    dplyr::mutate(feature = forcats::fct_reorder(feature, -importance)) %>%
+    ggplot2::ggplot(ggplot2::aes(x = feature, weight = importance)) +
+    ggplot2::geom_bar()
 }
